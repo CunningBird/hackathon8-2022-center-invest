2ff1708894629075e2531cdf794da306b5009383
CunningBird <68229355+CunningBird@users.noreply.github.com>
2022-04-15 21:52:36 +0300

    Create GarageClient

--- buyer/src/main/kotlin/com/example/buyer/client/GarageClient.kt
+++ buyer/src/main/kotlin/com/example/buyer/client/GarageClient.kt
@@ -1,9 +1,19 @@
 package com.example.buyer.client
 
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
-@FeignClient(value = "cat", url = "http://localhost:8078/")
+@FeignClient(value = "garage", url = "http://localhost:8078/")
 interface GarageClient {
 
-    fun
+    @GetMapping("/list")
+    fun list(): List<String>
+
+    @GetMapping("/test")
+    fun testGarage(@RequestParam title: String): String
+
+    @PostMapping("/buy")
+    fun buyGarage(@RequestParam title: String): String
 }
